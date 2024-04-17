997529f816dec03ed790abb3102cd0ef8851072e
Alexei Shatov <alexei.shatov@gmail.com>
2024-04-17 16:42:28 +0300

    Some corrections

--- app/src/main/java/com/shatov/super_sanbot/MainActivity.kt
+++ app/src/main/java/com/shatov/super_sanbot/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sanbot.opensdk.base.BindBaseActivity
+
 import com.sanbot.opensdk.beans.FuncConstant
 import com.sanbot.opensdk.beans.FuncConstant.HARDWARE_MANAGER
-
 import com.sanbot.opensdk.function.unit.HardWareManager
 
 
 class MainActivity : AppCompatActivity() {
+    //private HardWareManager hardWareManager;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,9 +25,12 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        HardWareManager hardWareManager = getUnitManager(HARDWARE_MANAGER) as HardWareManager
-
+        //HardWareManager hardWareManager = (HardWareManager)getUnitManager
+               // (FuncConstant.HARDWARE_MANAGER);
+        //HardWareManager hardWareManager = getUnitManager(HARDWARE_MANAGER) as HardWareManager
         //hardWareManager = (HardWareManager) getUnitManager(FuncConstant.HARDWARE_MANAGER);
+
+        val hardWareManager = getUnitManager(HARDWARE_MANAGER) as HardWareManager
 
         hardWareManager.setOnHareWareListener(new GyroscopeListener() {
             @Override public void gyroscopeData(float driftAngle, float elevationAngle, float rollAngle) {
